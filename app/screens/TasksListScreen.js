f7f3c46bf8dac24d2207497240c344894c7b9b91
frntEndDvlpr <reactdvlpr@gmail.com>
2024-07-09 16:42:29 +0400

    Add add button to Task Listing Screen

--- app/screens/TasksListScreen.js
+++ app/screens/TasksListScreen.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 import ListItem from "../components/ListItem";
 import ListItemSeparator from "../components/ListItemSeparator";
 import ListItemDeleteAction from "../components/ListItemDeleteAction";
+import colors from "../config/colors";
 
 const initialTasks = [
   {
@@ -37,39 +38,57 @@ function TasksListScreen(props) {
   };
 
   return (
-    <FlatList
-      data={initialTasks}
-      keyExtractor={(task) => task.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          date={item.date}
-          assignee={item.assignee}
-          project={item.project}
-          customer={item.customer}
-          image={item.image}
-          onPress={() => console.log("Task Selected", item)}
-          renderRightActions={() => (
-            <ListItemDeleteAction onPress={() => handleDelete(item)} />
-          )}
-        />
-      )}
-      ItemSeparatorComponent={ListItemSeparator}
-      refreshing={refreshing}
-      onRefresh={() => {
-        setTasks([
-          {
-            id: 2,
-            title: "Task 2",
-            date: "6/19/2024",
-            assignee: "Ahmed",
-            project: "Project 2",
-            customer: "ABC",
-          },
-        ]);
-      }}
-    />
+    <>
+      <FlatList
+        data={initialTasks}
+        keyExtractor={(task) => task.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            date={item.date}
+            assignee={item.assignee}
+            project={item.project}
+            customer={item.customer}
+            image={item.image}
+            onPress={() => console.log("Task Selected", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setTasks([
+            {
+              id: 2,
+              title: "Task 2",
+              date: "6/19/2024",
+              assignee: "Ahmed",
+              project: "Project 2",
+              customer: "ABC",
+            },
+          ]);
+        }}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>+</Text>
+      </TouchableOpacity>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    backgroundColor: colors.primary,
+    height: 50,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 30,
+  },
+  text: { fontSize: 30, color: colors.white },
+});
 
 export default TasksListScreen;
