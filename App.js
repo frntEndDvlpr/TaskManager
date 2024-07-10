2f5339818f6bfa8f9ed68d00bac8af7bf4663c34
frntEndDvlpr <reactdvlpr@gmail.com>
2024-07-10 12:04:46 +0400

    create ProjectsFormScreen

--- App.js
+++ App.js
@@ -2,14 +2,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 
 import AuthNavigator from "./app/navigation/AuthNavigator";
-import ClientScreen from "./app/screens/ClientScreen";
+import ProjectsFormScreen from "./app/screens/ProjectsFormScreen";
 import AppNavigator from "./app/navigation/AppNavigator";
 import NavigationTheme from "./app/navigation/NavigationTheme";
 
 export default function App() {
   return (
     <NavigationContainer theme={NavigationTheme}>
-      <AuthNavigator />
+      <ProjectsFormScreen />
     </NavigationContainer>
   );
 }
