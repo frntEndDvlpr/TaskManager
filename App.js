207a6f1590eabda9ed3214efa1a69b497c018c8a
frntEndDvlpr <reactdvlpr@gmail.com>
2024-11-19 14:03:55 +0400

    after maintain

--- App.js
+++ App.js
@@ -42,22 +42,6 @@ const App = () => {
     }
   };
 
-  const isWithinOneKm = (lat1, lon1, lat2, lon2) => {
-    const toRad = (value) => (value * Math.PI) / 180;
-    const R = 6371; // Radius of the Earth in km
-    const dLat = toRad(lat2 - lat1);
-    const dLon = toRad(lon2 - lon1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(lat1)) *
-        Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in km
-    return distance <= 1;
-  };
-
   return (
     <View style={styles.container}>
       <Text>
