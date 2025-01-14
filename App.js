724123b56b1c178bd51c85188c2f2f570bccf694
frntEndDvlpr <reactdvlpr@gmail.com>
2025-01-14 07:50:23 +0400

    CSS

--- App.js
+++ App.js
@@ -59,7 +59,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+     alignItems: "center",
     justifyContent: "center",
     padding: 20,
   },
